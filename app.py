96e1e2cfc355b6b9f1757203c888b82d783af0b6
ManojhR <145860619+manojh17@users.noreply.github.com>
2026-07-09 23:47:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -696,4 +696,4 @@ def list_sales():
 
 # ── Run ───────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False,host="0.0.0.0", port=5000)
